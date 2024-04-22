b7c4215b38b21b724e381b5d5800f27c0fdff487
劉晏儒 <ruru@liuyanrudeMacBook-Pro.local>
2024-04-22 22:23:48 +0800

    update

--- week4/main.py
+++ week4/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Request,Form,Path,Query
+from fastapi import FastAPI,Request,Form,Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -37,12 +37,12 @@ async def index(request: Request):
 async def signIn(request:Request,username:str=Form(None),password:str=Form(None)):
     if not username or not password:
         error_message="Please enter username and password"
-        return RedirectResponse(url=f"/error/{error_message}", status_code=303)
+        return RedirectResponse(url=f"/error?message={error_message}", status_code=303)
     elif verify(request,username,password):
             return RedirectResponse(url="/member", status_code=303)
     else:
         error_message="Username or password is not correct"
-        return RedirectResponse(url=f"/error/{error_message}", status_code=303)
+        return RedirectResponse(url=f"/error?message={error_message}", status_code=303)
 @app.route("/member",methods=["GET", "POST"])
 async def member(request: Request):
     if request.method == "POST":
@@ -53,9 +53,9 @@ async def member(request: Request):
         else:
             return templates.TemplateResponse("./static/member.html", {"request": request})
 
-@app.get("/error/{error_message}")
-async def error(request: Request, error_message: str):
-    return templates.TemplateResponse("./static/error.html", {"request": request, "message": error_message})
+@app.get("/error")
+async def error(request: Request ,message: str):
+    return templates.TemplateResponse("./static/error.html", {"request": request, "message": message})
 
 @app.get("/singout")
 async def singout(request: Request):
